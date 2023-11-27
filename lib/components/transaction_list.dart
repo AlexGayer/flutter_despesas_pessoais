03fecfe1c00ae54fcc16cca236c5f3141b3cffb3
Alexandre Gayer da Silva <alexandregs119@hotmail.com>
2023-11-27 14:07:05 -0300

    Charts

--- lib/components/transaction_list.dart
+++ lib/components/transaction_list.dart
@@ -10,60 +10,57 @@ class TransactionList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      height: 500,
-      child: transactions.isEmpty
-          ? Center(
-              child: Column(
-                children: [
-                  const SizedBox(height: 20),
-                  Text(
-                    "Nenhuma transação Cadastrada",
+    return transactions.isEmpty
+        ? Center(
+            child: Column(
+              children: [
+                const SizedBox(height: 20),
+                Text(
+                  "Nenhuma transação Cadastrada",
+                  style: Theme.of(context).textTheme.titleLarge,
+                ),
+                const SizedBox(height: 20),
+                SizedBox(
+                  height: 300,
+                  child: Image.asset(
+                    'assets/images/waiting.png',
+                    fit: BoxFit.cover,
+                  ),
+                )
+              ],
+            ),
+          )
+        : ListView.builder(
+            itemCount: transactions.length,
+            shrinkWrap: true,
+            itemBuilder: (context, index) {
+              final tr = transactions[index];
+              return Card(
+                elevation: 5,
+                margin: const EdgeInsets.symmetric(vertical: 8, horizontal: 5),
+                child: ListTile(
+                  trailing: IconButton(
+                      onPressed: () => removeTransaction(tr.id),
+                      icon: Icon(
+                        Icons.delete,
+                        color: Theme.of(context).colorScheme.error,
+                      )),
+                  leading: CircleAvatar(
+                    radius: 30,
+                    child: Padding(
+                      padding: const EdgeInsets.all(6.0),
+                      child: FittedBox(child: Text("R\$ ${tr.value}")),
+                    ),
+                  ),
+                  title: Text(
+                    tr.title,
                     style: Theme.of(context).textTheme.titleLarge,
                   ),
-                  const SizedBox(height: 20),
-                  SizedBox(
-                    height: 300,
-                    child: Image.asset(
-                      'assets/images/waiting.png',
-                      fit: BoxFit.cover,
-                    ),
-                  )
-                ],
-              ),
-            )
-          : ListView.builder(
-              itemCount: transactions.length,
-              shrinkWrap: true,
-              itemBuilder: (context, index) {
-                final tr = transactions[index];
-                return Card(
-                  elevation: 5,
-                  margin: const EdgeInsets.symmetric(vertical: 8, horizontal: 5),
-                  child: ListTile(
-                    trailing: IconButton(
-                        onPressed: () => removeTransaction(tr.id),
-                        icon: Icon(
-                          Icons.delete,
-                          color: Theme.of(context).colorScheme.error,
-                        )),
-                    leading: CircleAvatar(
-                      radius: 30,
-                      child: Padding(
-                        padding: const EdgeInsets.all(6.0),
-                        child: FittedBox(child: Text("R\$ ${tr.value}")),
-                      ),
-                    ),
-                    title: Text(
-                      tr.title,
-                      style: Theme.of(context).textTheme.titleLarge,
-                    ),
-                    subtitle: Text(
-                      DateFormat("dd/MM/yyyy").format(tr.data),
-                    ),
+                  subtitle: Text(
+                    DateFormat("dd/MM/yyyy").format(tr.data),
                   ),
-                );
-              }),
-    );
+                ),
+              );
+            });
   }
 }
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -52,18 +52,27 @@ class _HomePageState extends State<HomePage> {
 
   @override
   Widget build(BuildContext context) {
+    final appBar = AppBar(
+      title: const Text("Despesas Pessoais"),
+      actions: [IconButton(onPressed: () => _openTransactionFormModal(context), icon: const Icon(Icons.add))],
+    );
+    final availableHeight = MediaQuery.of(context).size.height - appBar.preferredSize.height - MediaQuery.of(context).padding.top;
+
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("Despesas Pessoais"),
-        actions: [IconButton(onPressed: () => _openTransactionFormModal(context), icon: const Icon(Icons.add))],
-      ),
+      appBar: appBar,
       body: SingleChildScrollView(
         child: Column(
           children: [
-            Chart(recentTransactions: _recentTransactions),
-            TransactionList(
-              transactions: _transactions,
-              removeTransaction: _removeTransaction,
+            SizedBox(
+              height: availableHeight * 0.30,
+              child: Chart(recentTransactions: _recentTransactions),
+            ),
+            SizedBox(
+              height: availableHeight * 0.75,
+              child: TransactionList(
+                transactions: _transactions,
+                removeTransaction: _removeTransaction,
+              ),
             ),
           ],
         ),
